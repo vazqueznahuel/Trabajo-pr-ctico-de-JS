ca4f719b6ea4b26a2744158213d3c6a80ec41713
vazqueznahuel <vazqueznahuel.epetn20qgmail.com>
2022-10-04 13:07:39 -0300

    nueva region de funciones y variables, con sus respectivos comentarios

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 //#region 
-//en esta region estaran las funciones basicas de matematicas
+    //en esta region estaran las variables
+//#endregion
+//#region 
+    //en esta region estaran las funciones basicas matematicas
 //#endregion
