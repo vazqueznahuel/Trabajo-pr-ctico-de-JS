bb7e3d86358bbee82455be2f998ba0c2f4585578
vazqueznahuel <vazqueznahuel.epetn20qgmail.com>
2022-10-04 13:36:48 -0300

    se agregaron las 4 operaciones basicas matematicas

--- script.js
+++ script.js
@@ -6,6 +6,10 @@
 //#endregion
 //#region 
     //en esta region estaran las funciones basicas matematicas
+    var suma = num => num + num;
+    let resta = num => num - num;
+    const multiplicacion = num => num * num;
+    var division = num => num / num;
 //#endregion
 //#region 
     //en esta region se imprimen los datos
