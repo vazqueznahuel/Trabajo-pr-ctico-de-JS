cb71c9a7689e59845207fb83760d13cab5ebd264
vazqueznahuel <vazqueznahuel.epetn20qgmail.com>
2022-10-04 14:37:53 -0300

    agregando array y su muestra en consola

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 //#region 
-    //en esta region estaran las variables
-    var nombre = "Adrian";
-    let edad = 18;
-    const apellido = "Vazquez";    
+//en esta region estaran las variables
+var nombre = "Adrian";
+let edad = 18;
+const apellido = "Vazquez";
+let value = 0;
+var arreglo = [1, 2, 3, 4, 5, 6];
 //#endregion
 //#region 
-    //en esta region estaran las funciones basicas matematicas
-    var suma = num => num + num;
-    let resta = num => num - num;
-    const multiplicacion = num => num * num;
-    var division = num => num / num;
+//en esta region estaran las funciones basicas matematicas
+var suma = num => num + num;
+let resta = num => num - num;
+const multiplicacion = num => num * num;
+var division = num => num / num;
 //#endregion
 //#region 
-    //en esta region se imprimen los datos
-    console.log ("Nombre : " + nombre);
-    console.log (`Apellido : ` + apellido);
-    console.log ('Edad : ' + Edad);
+//en esta region se imprimen los datos
+console.log("Nombre : " + nombre);
+console.log(`Apellido : ` + apellido);
+console.log('Edad : ' + edad);
+for (value of arreglo) {
+    console.log(arreglo[value]);
+    value++;
+}
 //#endregion
